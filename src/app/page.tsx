03a3df4d43e6bf0b67938995e111719c2a48b03d
Abisuwa Oluwadunsin <ayodejiabisuwa23@gmail.com>
2025-02-16 20:27:48 +0100

    added logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,26 @@
 import Image from "next/image";
-import HowItWorks from "@/components/OnePager/HowItWorks"
-import Intro from "@/components/OnePager/Intro"
-import Navbar from "@/components/OnePager/Navbar"
-import Map from "@/components/OnePager/Map"
+import HowItWorks from "@/components/OnePager/HowItWorks";
+import Intro from "@/components/OnePager/Intro";
+import Navbar from "@/components/OnePager/Navbar";
+import Map from "@/components/OnePager/Map";
 import { TracingBeam } from "@/components/ui/tracing-beam";
-
+import ColourfulText from "@/components/ui/colourful-text";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-items-center min-h-screen  pb-20 gap-16 sm:p-20">
       {/* <TracingBeam className="">  */}
-    <Navbar/>
-    <Intro/>
-    <HowItWorks/>
-    <Map/>
-    {/* </TracingBeam> */}
+      <div className="flex justify-between">
+        <h1 className="text-7xl font-bold text-center text-black relative z-2 font-sans">
+          <ColourfulText text="GridX" />
+        </h1>
+        <Navbar />
+      </div>
+
+      <Intro />
+      <HowItWorks />
+      <Map />
+      {/* </TracingBeam> */}
     </div>
   );
 }
--- src/components/OnePager/ColorfulText.tsx
+++ src/components/OnePager/ColorfulText.tsx
@@ -8,14 +8,14 @@ export default function ColourfulTextDemo() {
     <div className="h-screen w-full flex items-center justify-center relative overflow-hidden bg-black">
       <motion.img
         src="grid.jpg"
-        className="h-full w-full object-cover absolute inset-0 [mask-image:radial-gradient(circle,transparent,black_80%)] pointer-events-none"
+        className="h-full w-full  object-cover absolute inset-0 [mask-image:radial-gradient(circle,transparent,black_80%)] pointer-events-none"
         initial={{ opacity: 0 }}
         animate={{ opacity: 0.5 }}
         transition={{ duration: 1 }}
       />
-      <h1 className="text-2xl md:text-3xl lg:text-7xl font-bold text-center text-white relative z-2 font-sans">
-        Powering a decentralized future with blockchain-based{" "}
-        <ColourfulText text=" peer-to-peer" /> <br />
+      <h1 className="text-2xl px-8 md:text-3xl lg:text-7xl font-bold text-center text-white relative z-2 font-sans">
+        Redefining energy ownership with blockchain-powered{" "}
+        <ColourfulText text="peer-to-peer" /> <br />
         energy trading.
       </h1>
     </div>
